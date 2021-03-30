9734223323e2af61dec45d34fd56f6523f505d6a
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-03-30 18:56:01 -0300

    List posts by Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import { RichText } from 'prismic-dom';
-import { FiUser, FiCalendar } from 'react-icons/fi';
-
+import { useState } from 'react';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -28,7 +30,39 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+
+  function handlePostsInNextPage() {
+    fetch(nextPage)
+      .then(response => response.json())
+      .then(response => {
+        const postsMore: Post[] = response.results.map(post => {
+          const dateFormatted = format(
+            new Date(post.first_publication_date),
+            'dd MMM yyyy',
+            {
+              locale: ptBR,
+            }
+          );
+
+          return {
+            uid: post.uid,
+            data: {
+              author: RichText.asText(post.data.author),
+              subtitle: RichText.asText(post.data.subtitle),
+              title: RichText.asText(post.data.title),
+            },
+            first_publication_date: dateFormatted,
+          };
+        });
+
+        setPosts([...posts, ...postsMore]);
+        setNextPage(response.next_page);
+      });
+  }
+
   return (
     <>
       <Head>
@@ -37,115 +71,84 @@ export default function Home(): JSX.Element {
 
       <main className={`${commonStyles.container} ${styles.container}`}>
         <div className={commonStyles.content}>
-          <img
-            className={styles.logo}
-            src="/images/logo.svg"
-            alt="Space Traveling"
-          />
+          <img className={styles.logo} src="/images/logo.svg" alt="logo" />
 
           <ul className={styles.posts}>
-            <li>
-              <h1>Como utilizar hooks</h1>
-              <sub>Pensando em sincronização em vez de ciclos de vida.</sub>
-              <footer>
-                <time>
-                  <FiCalendar />
-                  15 Mar 2021
-                </time>
-                <p>
-                  <FiUser />
-                  Joseph Oliveira
-                </p>
-              </footer>
-            </li>
-            <li>
-              <h1>Como utilizar hooks</h1>
-              <sub>Pensando em sincronização em vez de ciclos de vida.</sub>
-              <footer>
-                <time>
-                  <FiCalendar />
-                  15 Mar 2021
-                </time>
-                <p>
-                  <FiUser />
-                  Joseph Oliveira
-                </p>
-              </footer>
-            </li>
-            <li>
-              <h1>Como utilizar hooks</h1>
-              <sub>Pensando em sincronização em vez de ciclos de vida.</sub>
-              <footer>
-                <time>
-                  <FiCalendar />
-                  15 Mar 2021
-                </time>
-                <p>
-                  <FiUser />
-                  Joseph Oliveira
-                </p>
-              </footer>
-            </li>
-            <li>
-              <h1>Como utilizar hooks</h1>
-              <sub>Pensando em sincronização em vez de ciclos de vida.</sub>
-              <footer>
-                <time>
-                  <FiCalendar />
-                  15 Mar 2021
-                </time>
-                <p>
-                  <FiUser />
-                  Joseph Oliveira
-                </p>
-              </footer>
-            </li>
+            {posts.map(post => (
+              <li key={post.uid}>
+                <Link href={`/post/${post.uid}`}>
+                  <a>
+                    <h1>{post.data.title}</h1>
+                    <sub>{post.data.subtitle}</sub>
+                    <footer>
+                      <time>
+                        <FiCalendar />
+                        {post.first_publication_date}
+                      </time>
+                      <p>
+                        <FiUser />
+                        {post.data.author}
+                      </p>
+                    </footer>
+                  </a>
+                </Link>
+              </li>
+            ))}
           </ul>
 
-          <button className={styles.morePosts} type="button">
-            Carregar mais posts
-          </button>
+          {nextPage?.length && (
+            <button
+              onClick={handlePostsInNextPage}
+              className={styles.morePosts}
+              type="button"
+            >
+              Carregar mais posts
+            </button>
+          )}
         </div>
       </main>
     </>
   );
 }
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const prismic = getPrismicClient();
-//   const postsResponse = await prismic.query(
-//     [Prismic.predicates.at('document.type', 'posts')],
-//     {
-//       fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
-//       page: 1,
-//       pageSize: 20,
-//     }
-//   );
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   const results: Post[] = postsResponse.results.map(post => {
-//     return {
-//       uid: post.uid,
-//       data: {
-//         author: RichText.asText(post.data.author),
-//         subtitle: RichText.asText(post.data.subtitle),
-//         title: RichText.asText(post.data.title),
-//       },
-//       first_publication_date: new Date(
-//         post.last_publication_date
-//       ).toLocaleDateString('pt-BR', {
-//         day: '2-digit',
-//         month: 'long',
-//         year: 'numeric',
-//       }),
-//     };
-//   });
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      page: 1,
+    }
+  );
 
-//   const posts: PostPagination = {
-//     next_page: '1',
-//     results,
-//   };
+  const posts: Post[] = postsResponse.results.map(post => {
+    const dateFormatted = format(
+      new Date(post.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    );
 
-//   return {
-//     props: { posts },
-//   };
-// };
+    return {
+      uid: post.uid,
+      data: {
+        author: RichText.asText(post.data.author),
+        subtitle: RichText.asText(post.data.subtitle),
+        title: RichText.asText(post.data.title),
+      },
+      first_publication_date: dateFormatted,
+    };
+  });
+
+  const postsPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  return {
+    props: { postsPagination },
+    revalidate: 60 * 60, // 1 hour
+  };
+};
